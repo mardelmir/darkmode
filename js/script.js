3e950930438a5c3254618f813c8c7d1b5eeaa898
María Delgado <mariad1996@gmail.com>
2023-12-08 16:28:22 +0100

    finished

--- js/script.js
+++ js/script.js
@@ -3,36 +3,30 @@ const darkBtn = document.getElementById('dark-btn')
 
 lightBtn.addEventListener('click', () => {
     lightBtn.classList.add('btn-move-light')
-    setTimeout(() => { 
-        lightBtn.classList.toggle('hide') 
+    setTimeout(() => {
         darkBtn.classList.toggle('hide')
+        lightBtn.classList.toggle('hide')
         lightBtn.classList.remove('btn-move-light')
-    }, 200)
+    }, 130)
     changeMode()
 })
 
 darkBtn.addEventListener('click', () => {
     darkBtn.classList.add('btn-move-dark')
-    setTimeout(() => { 
-        darkBtn.classList.toggle('hide') 
+    setTimeout(() => {
         lightBtn.classList.toggle('hide')
+        darkBtn.classList.toggle('hide')
         darkBtn.classList.remove('btn-move-dark')
-    }, 200)
+    }, 130)
     changeMode()
 })
 
 const changeMode = () => {
-    const body = document.getElementsByTagName('body')[0]
-    body.classList.toggle('black-background')
+    document.getElementsByTagName('body')[0].classList.toggle('black-background')
 
     const yellow = [document.getElementsByTagName('h1')[0], document.getElementsByTagName('h2')[0]]
-    for (let text of yellow) {
-        text.classList.toggle('yellow-text')
-    }
+    yellow.forEach((title => title.classList.toggle('yellow-text')))
 
     const white = [document.getElementById('dark-mode-title'), document.querySelector('.div-text')]
-    for (let text of white) {
-        text.classList.toggle('white-text')
-    }
+    white.forEach((text => text.classList.toggle('white-text')))
 }
-
